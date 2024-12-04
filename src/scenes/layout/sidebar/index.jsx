c6a1c198b09b5b980d5d976f090e9549d6443ad1
bruno valu <brunodesigner3@gmail.com>
2024-12-03 23:06:43 -0300

    first commit

--- src/scenes/layout/sidebar/index.jsx
+++ src/scenes/layout/sidebar/index.jsx
@@ -147,7 +147,7 @@ const SideBar = () => {
         </Menu>
         <Typography
           variant="h6"
-          color={colors.gray[300]}
+          color={colors.gray[400]}
           sx={{ m: "15px 0 5px 20px" }}
         >
           {!collapsed ? "Data" : " "}
