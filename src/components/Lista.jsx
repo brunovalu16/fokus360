92b9694f3423196133d454350a808086d0503975
brunovalu16 <brunodesigner3@gmail.com>
2024-12-19 18:19:19 -0300

    criando componetes

---
+++ src/components/Lista.jsx
@@ -0,0 +1,304 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  IconButton,
+  Typography,
+  Checkbox,
+  FormControlLabel,
+} from "@mui/material";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridToolbarFilterButton,
+  GridToolbarColumnsButton,
+} from "@mui/x-data-grid";
+import { mockDataProjects } from "../data/mockData";
+import { tokens } from "../theme";
+import { useNavigate } from "react-router-dom";
+import PermContactCalendarIcon from "@mui/icons-material/PermContactCalendar";
+import DeleteForeverSharpIcon from "@mui/icons-material/DeleteForeverSharp";
+import { useTheme } from "@mui/material/styles";
+
+
+// Tradução dos textos da Toolbar e rodapé
+const localeText = {
+  toolbarColumns: "Colunas",
+  toolbarFilters: "Filtros",
+  toolbarDensity: "Densidade",
+  toolbarExport: "Exportar",
+  footerRowSelected: (count) => (count === 1 ? `` : ``),
+  footerTotalRows: "Linhas totais:",
+  footerPaginationRowsPerPage: "Linhas por página:",
+};
+
+// Barra de ferramentas personalizada
+const CustomToolbar = () => (
+  <GridToolbarContainer
+  sx={{
+    padding: "8px",
+    borderRadius: "5px",
+    backgroundColor: "#f4f6f8",
+    marginBottom: "10px",
+    overflow: "visible", // Garante que o conteúdo extrapole os limites do contêiner
+    position: "relative", // Define o contexto posicional
+    zIndex: 1, // Eleva o contêiner para evitar sobreposição de outros elementos
+  }}
+>
+  <GridToolbarColumnsButton
+    sx={{
+      color: "#727681",
+      "&:hover": { color: "#3f51b5" },
+      zIndex: 2, // Garante que o botão fique acima de outros elementos
+    }}
+  />
+  <GridToolbarFilterButton
+  sx={{
+    paddingTop: "10px",
+    color: "#727681",
+    "&:hover": { color: "#3f51b5" },
+    overflow: "visible", // Garante que o conteúdo extrapole
+    position: "relative", // Contexto posicional para o tooltip
+  }}
+/>
+
+  <GridToolbarExport
+    sx={{
+      color: "#727681",
+      "&:hover": { color: "#3f51b5" },
+      zIndex: 2, // Eleva o botão exportar também
+    }}
+  />
+</GridToolbarContainer>
+
+
+);
+
+const Lista = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const navigate = useNavigate();
+  const [checkedRows, setCheckedRows] = useState({});
+
+  const handleDelete = (id) => console.log(`Excluir projeto com ID: ${id}`);
+  const handleNavigateToProject = (id) => navigate(`/projeto?id=${id}`);
+  const handleCheckboxChange = (id, isChecked) =>
+    setCheckedRows((prev) => ({ ...prev, [id]: isChecked }));
+
+  const columns = [
+    {
+      field: "name",
+      headerName: "Nome do projeto",
+      flex: 1.1,
+      cellClassName: "name-column--cell",
+    },
+    { field: "solicitante", headerName: "Solicitante", flex: 1 },
+    {
+      field: "startDate",
+      headerName: "Início",
+      flex: 0.8,
+      renderCell: (params) => {
+        const [day, month, year] = params.row.startDate.split("/");
+        const [deadlineDay, deadlineMonth, deadlineYear] = params.row.deadline.split("/");
+
+        const startDate = new Date(Date.UTC(year, month - 1, day));
+        const deadlineDate = new Date(
+          Date.UTC(deadlineYear, deadlineMonth - 1, deadlineDay)
+        );
+
+        let backgroundColor;
+
+        if (startDate > deadlineDate) {
+          backgroundColor = "#f44336";
+        } else if (startDate.getTime() === deadlineDate.getTime()) {
+          backgroundColor = "#3f51b5";
+        } else if ((deadlineDate - startDate) / (1000 * 60 * 60 * 24) <= 2) {
+          backgroundColor = "#FFC107";
+        } else {
+          backgroundColor = "#4CAF50";
+        }
+
+        return (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+              maxWidth: "100%",
+              minWidth: "100%",
+              justifyContent: "flex-start",
+              fontWeight: "bold",
+            }}
+          >
+            <Box
+              sx={{
+                width: "10px",
+                height: "10px",
+                borderRadius: "50%",
+                backgroundColor: backgroundColor || "#583cff",
+                marginRight: "5px",
+              }}
+            />
+            {startDate.toLocaleDateString("pt-BR", { timeZone: "UTC" })}
+          </Box>
+        );
+      },
+    },
+    {
+      field: "deadline",
+      headerName: "Prazo previsto",
+      flex: 0.8,
+      renderCell: (params) => {
+        const [day, month, year] = params.value.split("/");
+        const deadlineDate = new Date(Date.UTC(year, month - 1, day));
+
+        return (
+          <Box>
+            {deadlineDate.toLocaleDateString("pt-BR", { timeZone: "UTC" })}
+          </Box>
+        );
+      },
+    },
+    {
+      field: "currentSpending",
+      headerName: "Gasto atual",
+      flex: 0.9,
+      renderCell: (params) => {
+        const currentSpending = parseFloat(
+          params.row.currentSpending.replace(/[^0-9.-]+/g, "")
+        );
+        const budget = parseFloat(params.row.budget.replace(/[^0-9.-]+/g, ""));
+
+        let backgroundColor = "#4CAF50";
+        if (currentSpending === budget) backgroundColor = "#583cff";
+        else if (currentSpending >= budget) backgroundColor = "#f44336";
+        else if (currentSpending >= budget * 0.8) {
+          backgroundColor = "#FFC107";
+        }
+
+        return (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+              maxWidth: "100%",
+              minWidth: "100%",
+              justifyContent: "flex-start",
+              fontWeight: "bold",
+            }}
+          >
+            <Box
+              sx={{
+                width: "10px",
+                height: "10px",
+                borderRadius: "50%",
+                backgroundColor: backgroundColor || "#583cff",
+                marginRight: "5px",
+              }}
+            />
+            {currentSpending.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </Box>
+        );
+      },
+    },
+    {
+      field: "budget",
+      headerName: "Orçamento",
+      flex: 0.7,
+      renderCell: (params) => (
+        <Box>
+          {parseFloat(params.row.budget.replace(/[^0-9.-]+/g, "")).toLocaleString(
+            "pt-BR",
+            {
+              style: "currency",
+              currency: "BRL",
+            }
+          )}
+        </Box>
+      ),
+    },
+    {
+      field: "active",
+      headerName: "Ativo",
+      flex: 0.4,
+      renderCell: (params) => (
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={!!checkedRows[params.row.id]}
+              onChange={(event) =>
+                handleCheckboxChange(params.row.id, event.target.checked)
+              }
+              sx={{ color: "#312783", "&.Mui-checked": { color: "#312783" } }}
+            />
+          }
+        />
+      ),
+    },
+    {
+      field: "actions",
+      headerName: "Ações",
+      flex: 0.9,
+      renderCell: ({ row }) => (
+        <Box display="flex" gap={1}>
+          <Button
+            size="small"
+            onClick={() => handleNavigateToProject(row.id)}
+            sx={{
+              color: "#fff",
+              backgroundColor: "#583cff",
+              marginBottom: "5px",
+              marginTop: "5px",
+              fontSize: "0.65rem",
+              "&:hover": { backgroundColor: "#3f2cb2" },
+            }}
+          >
+            Editar
+          </Button>
+          <IconButton onClick={() => handleDelete(row.id)}>
+            <DeleteForeverSharpIcon
+              sx={{ fontSize: 25, color: colors.redAccent[600] }}
+            />
+          </IconButton>
+        </Box>
+      ),
+    },
+  ];
+
+  return (
+    <>
+
+      <DataGrid
+        rows={mockDataProjects}
+        columns={columns}
+        components={{ Toolbar: CustomToolbar }}
+        localeText={localeText}
+        initialState={{ pagination: { paginationModel: { pageSize: 5 } } }}
+        sx={{
+          marginLeft: "-13px",
+          marginTop: "5px",
+          width: "calc(100% - -10px)",
+          minHeight: "50vh",
+          padding: "15px",
+          paddingLeft: "30px",
+          borderRadius: "20px",
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+          bgcolor: "#f2f0f0",
+          overflowX: "hidden",
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: "#312783",
+            color: "#bcbcbc",
+            fontSize: "13px",
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default Lista;
--- src/scenes/listaprojetos/index.jsx
+++ src/scenes/listaprojetos/index.jsx
@@ -1,274 +1,56 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  IconButton,
-  useTheme,
-  Typography,
-  Checkbox,
-  FormControlLabel,
-} from "@mui/material";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import DeleteForeverSharpIcon from "@mui/icons-material/DeleteForeverSharp";
+import React from "react";
+import { Box, Typography, Button, } from "@mui/material";
 import PermContactCalendarIcon from "@mui/icons-material/PermContactCalendar";
-import { DataGrid, GridToolbarContainer, GridToolbarExport, GridToolbarFilterButton, GridToolbarDensitySelector, GridToolbarColumnsButton } from "@mui/x-data-grid";
-import { Header } from "../../components";
-import { mockDataProjects } from "../../data/mockData";
+import Lista from "../../components/Lista";
+import { useTheme } from "@mui/material/styles";
 import { tokens } from "../../theme";
-import { useNavigate } from "react-router-dom";
 
-// Tradução dos textos da Toolbar e rodapé
-const localeText = {
-  toolbarColumns: "Colunas",
-  toolbarFilters: "Filtros",
-  toolbarDensity: "Densidade",
-  toolbarExport: "Exportar",
-  footerRowSelected: (count) =>
-  count === 1 ? `` : ``,
-  footerTotalRows: "Linhas totais:",
-  footerPaginationRowsPerPage: "Linhas por página:",
-  
-};
+const Projetos = () => {
 
-// Barra de ferramentas personalizada
-const CustomToolbar = () => (
-  <GridToolbarContainer
-    sx={{
-      padding: "8px",
-      borderRadius: "5px",
-      backgroundColor: "#f4f6f8",
-      marginBottom: "10px",
-    }}
-  >
-    <GridToolbarColumnsButton sx={{ color: "#727681", "&:hover": { color: "#3f51b5" } }} />
-    <GridToolbarFilterButton sx={{ color: "#727681", "&:hover": { color: "#3f51b5" } }} />
-    {/**   <GridToolbarDensitySelector sx={{ color: "#727681", "&:hover": { color: "#3f51b5" } }} />   */}
-    <GridToolbarExport sx={{ color: "#727681", "&:hover": { color: "#3f51b5" } }} />
-  </GridToolbarContainer>
-);
+  const theme = useTheme(); // Inicializa o tema
+  const colors = tokens(theme.palette.mode); // Obtém as cores do tema atual
 
-const ListaProjetos = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const navigate = useNavigate();
-  const [checkedRows, setCheckedRows] = useState({});
-
-  const handleDelete = (id) => console.log(`Excluir projeto com ID: ${id}`);
-  const handleNavigateToProject = (id) => navigate(`/projeto?id=${id}`);
-  const handleCheckboxChange = (id, isChecked) =>
-    setCheckedRows((prev) => ({ ...prev, [id]: isChecked }));
-
-  const formatDate = (dateString) => {
-    if (!dateString) return ""; // Evita erros com datas vazias
-  
-    const [day, month, year] = dateString.split("/"); // Divide no formato dd/mm/yyyy
-    const parsedDate = new Date(`${year}-${month}-${day}`); // Cria a data no formato yyyy-mm-dd
-  
-    if (isNaN(parsedDate)) return "Data Inválida"; // Verifica se a data é válida
-  
-    return parsedDate.toLocaleDateString("pt-BR", { year: "numeric", month: "2-digit", day: "2-digit" });
-  };
-  
-
-  const columns = [
-    { field: "name",
-      headerName: "Nome do projeto",
-      flex: 1.1,
-      cellClassName: "name-column--cell"
-    },
-
-    { field: "solicitante",
-      headerName: "Solicitante",
-      flex: 1
-    },
-    {
-      field: "startDate",
-      headerName: "Início",
-      flex: 0.8,
-      renderCell: (params) => {
-        const [day, month, year] = params.row.startDate.split("/");
-        const [deadlineDay, deadlineMonth, deadlineYear] = params.row.deadline.split("/");
-  
-        const startDate = new Date(Date.UTC(year, month - 1, day));
-        const deadlineDate = new Date(Date.UTC(deadlineYear, deadlineMonth - 1, deadlineDay));
-  
-        let backgroundColor;
-  
-        // Corrige a lógica para identificar as datas passadas
-        if (startDate > deadlineDate) {
-          backgroundColor = "#f44336"; // Vermelho (startDate passou do prazo deadline)
-        } else if (startDate.getTime() === deadlineDate.getTime()) {
-          backgroundColor = "#3f51b5"; // Azul (datas iguais)
-        } else if ((deadlineDate - startDate) / (1000 * 60 * 60 * 24) <= 2) {
-          backgroundColor = "#FFC107"; // Amarelo (próximo do deadline)
-        } else {
-          backgroundColor = "#4CAF50"; // Verde (dentro do prazo)
-        }
-  
-        return (
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center", // Alinha verticalmente
-              gap: "8px", // Espaço entre a bolinha e o texto
-              maxWidth: "100%",
-              minWidth: "100%",
-              justifyContent: "flex-start", // Centraliza horizontalmente
-              fontWeight: "bold",
-            }}
-          >
-            {/* Bolinha */}
-            <Box
-              sx={{
-                width: "10px", // Define o tamanho da bolinha
-                height: "10px",
-                borderRadius: "50%", // Faz o elemento ser um círculo
-                backgroundColor: backgroundColor || "#583cff", // Cor da bolinha
-                marginRight: "5px",
-              }}
-            />
-            {startDate.toLocaleDateString("pt-BR", { timeZone: "UTC" })}
-          </Box>
-        );
-      },
-    },
-    {
-      field: "deadline",
-      headerName: "Prazo previsto",
-      flex: 0.8,
-      renderCell: (params) => {
-        const [day, month, year] = params.value.split("/");
-        const deadlineDate = new Date(Date.UTC(year, month - 1, day));
-  
-        return (
-          <Box>
-            {deadlineDate.toLocaleDateString("pt-BR", { timeZone: "UTC" })}
-          </Box>
-        );
-      },
-    },
-    
-    {
-      field: "currentSpending",
-      headerName: "Gasto atual",
-      flex: 0.8,
-      renderCell: (params) => {
-        const currentSpending = parseFloat(params.row.currentSpending.replace(/[^0-9.-]+/g, ""));
-        const budget = parseFloat(params.row.budget.replace(/[^0-9.-]+/g, ""));
-
-        let backgroundColor = "#4CAF50";
-        let textColor = "#fff";
-        if (currentSpending === budget) backgroundColor = "#583cff";
-        else if (currentSpending >= budget) backgroundColor = "#f44336";
-        else if (currentSpending >= budget * 0.8) {
-          backgroundColor = "#FFC107";
-          textColor = "#fff";
-        }
-
-        const formatDate = (dateString) => {
-          const [day, month, year] = dateString.split("/");
-          const formattedDate = new Date(`${year}-${month}-${day}`); // ISO format
-          return formattedDate.toLocaleDateString("pt-BR", { year: "numeric", month: "2-digit", day: "2-digit" });
-        };
-        
-
-        return (
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center", // Alinha verticalmente
-              gap: "8px", // Espaço entre a bolinha e o texto
-              maxWidth: "100%",
-              minWidth: "100%",
-              justifyContent: "flex-start", // Centraliza horizontalmente
-              fontWeight: "bold",
-            }}
-          >
-            {/* Bolinha */}
-            <Box
-              sx={{
-                width: "10px", // Define o tamanho da bolinha
-                height: "10px",
-                borderRadius: "50%", // Faz o elemento ser um círculo
-                backgroundColor: backgroundColor || "#583cff", // Cor da bolinha
-                marginRight: "5px",
-              }}
-            />
-            {currentSpending.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
-          </Box>
-        );
-      },
-    },
-    {
-      field: "budget",
-      headerName: "Orçamento",
-      flex: 0.7,
-      renderCell: (params) => (
-        <Box>
-          {parseFloat(params.row.budget.replace(/[^0-9.-]+/g, "")).toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          })}
-        </Box>
-      ),
-    },
-    {
-      field: "active",
-      headerName: "Ativo",
-      flex: 0.4,
-      renderCell: (params) => (
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={!!checkedRows[params.row.id]}
-              onChange={(event) => handleCheckboxChange(params.row.id, event.target.checked)}
-              sx={{ color: "#312783", "&.Mui-checked": { color: "#312783" } }}
-            />
-          }
-        />
-      ),
-    },
-    {
-      field: "actions",
-      headerName: "Ações",
-      flex: 0.9,
-      renderCell: ({ row }) => (
-        <Box display="flex" gap={1}>
-          <Button
-            size="small"
-            onClick={() => handleNavigateToProject(row.id)}
-            sx={{
-              color: "#fff",
-              backgroundColor: "#583cff",
-              marginBottom: "5px",
-              marginTop: "5px",
-              fontSize: "0.65rem", // Tamanho da fonte
-              "&:hover": { backgroundColor: "#3f2cb2" },
-            }}
-          >
-            Editar
-          </Button>
-          <IconButton onClick={() => handleDelete(row.id)}>
-            <DeleteForeverSharpIcon sx={{ fontSize: 25, color: colors.redAccent[600] }} />
-          </IconButton>
-        </Box>
-      ),
-    },
-  ];
 
   return (
     <>
-      <Box sx={{ marginLeft: "40px", paddingTop: "50px" }}>
-        <Header
-          title={
-            <Box display="flex" alignItems="center" gap={1}>
-              <PermContactCalendarIcon sx={{ color: "#5f53e5", fontSize: 40 }} />
-              <Typography>GERENCIADOR DE PROJETOS</Typography>
-            </Box>
-          }
-        />
+      <Box
+        sx={{
+          marginRight: "45px",
+          display: "flex", // Define um layout flexível
+          justifyContent: "space-between", // Espaça os elementos horizontalmente
+          alignItems: "center", // Alinha verticalmente os elementos
+          marginLeft: "40px",
+          paddingTop: "50px",
+          paddingBottom: "40px",
+        }}
+      >
+              {/* Primeiro Box - Ícone e Título */}
+        <Box display="flex" alignItems="center" gap={1}>
+          <PermContactCalendarIcon sx={{ color: "#5f53e5", fontSize: 40 }} />
+          <Typography>GERENCIADOR DE PROJETOS</Typography>
+        </Box>
+
+        {/* Segundo Box - Botão */}
+        <Box>
+          <Button
+            variant="contained"
+            sx={{
+              marginTop: "10px",
+              fontSize: "10px",
+              fontWeight: "bold",
+              borderRadius: "5px",
+              padding: "10px 20px",
+              backgroundColor: colors.blueAccent[1000], // Mantido como no original
+              boxShadow: "none",
+              "&:hover": { backgroundColor: "#3f2cb2" },
+            }}
+          >
+            NOVO PROJETO
+          </Button>
+        </Box>
       </Box>
-      
+
+
       <Box
         sx={{
           marginLeft: "40px",
@@ -281,41 +63,12 @@ const ListaProjetos = () => {
           boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
           bgcolor: "#f2f0f0",
           overflowX: "hidden",
-          "& .MuiDataGrid-columnHeaders": {
-            backgroundColor: "#312783",
-            color: "#bcbcbc",
-            fontSize: "13px",
-          },
         }}
       >
-        <Box display="flex" justifyContent="flex-end" mb={2}>
-        <Button
-            variant="contained"
-            color="primary"
-            onClick={""}
-            sx={{
-              fontSize: "10px",
-              fontWeight: "bold",
-              borderRadius: "5px",
-              padding: "10px 20px",
-              backgroundColor: colors.blueAccent[1000],
-              boxShadow: "none",
-              "&:hover": { backgroundColor: "#3f2cb2" },
-            }}
-          >
-            Novo projeto
-          </Button>
-        </Box>
-        <DataGrid
-          rows={mockDataProjects}
-          columns={columns}
-          components={{ Toolbar: CustomToolbar }}
-          localeText={localeText}
-          initialState={{ pagination: { paginationModel: { pageSize: 5 } } }}
-        />
+        <Lista />
       </Box>
     </>
   );
 };
 
-export default ListaProjetos;
+export default Projetos;
