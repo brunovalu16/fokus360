815233ed21abaef77c9f472e2e244cadc6bcddaa
brunovalu16 <brunodesigner3@gmail.com>
2025-03-13 13:50:07 -0300

    remoção de form

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -4,7 +4,6 @@ import Dashboard from "./dashboard";
 import Team from "./team";
 import Invoices from "./invoices";
 import Contacts from "./contacts";
-import Form from "./form";
 import Calendar from "./calendar";
 import Bar from "./bar";
 import Line from "./line";
