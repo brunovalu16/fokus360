f3139fe4dcd0578bb431c4c8865ec05ac56e6d85
brunovalu16 <brunodesigner3@gmail.com>
2025-04-03 14:38:22 -0300

    correção cadastro 04

--- src/components/BaseDiretriz3.jsx
+++ src/components/BaseDiretriz3.jsx
@@ -767,7 +767,7 @@ const areaRolesMap = {
               }),
             });
           })
-        );
+        );  
       }
   
       alert("✅ Táticas salvas corretamente dentro das Estratégicas!");
