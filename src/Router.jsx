376285af857e6f6380ec457d9a7b86ea018554fb
brunovalu16 <brunodesigner3@gmail.com>
2025-04-04 11:09:37 -0300

    28

--- src/Router.jsx
+++ src/Router.jsx
@@ -58,7 +58,7 @@ const router = createBrowserRouter(
         { path: "/cadastro", element: <PrivateRoute><dashboardPlanejamento /></PrivateRoute> },
         { path: "/home", element: <PrivateRoute><Home /></PrivateRoute> },
         { path: "/dataplanejamento", element: <PrivateRoute><DataPlanejamento /></PrivateRoute> },
-        { path: "/dashboardplanejamento/:id", element: <PrivateRoute><DashboardPlanejamento /></PrivateRoute> },
+        { path: "/dashboardplanejamento", element: <PrivateRoute><DashboardPlanejamento /></PrivateRoute> },
         { path: "/dashboard", element: <PrivateRoute><Dashboard /></PrivateRoute> },
         { path: "/planejamento", element: <PrivateRoute><Planejamento /></PrivateRoute> },
         { path: "/user", element: <PrivateRoute><User /></PrivateRoute> },
